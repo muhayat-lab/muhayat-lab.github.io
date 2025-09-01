7d1876dbf9f322a4a17b2f68f47366a85396f7d8
Muhayat <moehayat@gmail.com>
2025-09-01 14:28:13 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://muhayat.github.io/',
+  site: 'https://muhayat-lab.github.io/', // <- domain baru (tanpa subfolder)
   integrations: [tailwind({ applyBaseStyles: true })],
 });
